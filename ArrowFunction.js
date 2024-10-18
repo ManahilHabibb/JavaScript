48bbeba158510270a475e8d9647c992672c802e5
ManahilHabibb <manahil5503@gmail.com>
2024-10-18 19:55:14 +0500

    updated

---
+++ ArrowFunction.js
@@ -0,0 +1,7 @@
+const number=[1,2,3,4]
+
+number.forEach((number)=>
+{
+    console.log(number*2);
+});
+
--- function.js
+++
@@ -1,6 +0,0 @@
-function myfunction ( name , message )
-{
-    console.log("Hello, " + name + "! " + message);
-}
-
-myfunction("Manahil", "Welcome");
