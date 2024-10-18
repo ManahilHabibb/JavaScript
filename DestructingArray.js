3c91b4a5f7d2d839e4544b0b7c1a5d7e2b5bad2c
ManahilHabibb <manahil5503@gmail.com>
2024-10-18 20:22:18 +0500

    updated

--- DestructingArray.js
+++
@@ -1,5 +0,0 @@
-const myObj=[1,2,3,4,4]
-
-const[y,z]=myObj;
-
-console.log(myObj.y);
---
+++ ObjectsInArray.js
@@ -0,0 +1,18 @@
+const myarray=[
+
+{
+    name:"Fahad",
+    age:21,
+    city:"isb"
+},
+
+
+{
+    name:"Manahil",
+    age:21,
+    city:"isb"
+}
+
+];
+
+console.log(myarray[0].age);
